9a01931250f06d502cb9f20edb70c46767a5a116
ragknarx <ruids_@hotmail.com>
2024-05-17 16:45:21 -0600

    Arreglar tipo de dato para decimal

--- MODELS/PlanesEntrenamiento.cs
+++ MODELS/PlanesEntrenamiento.cs
@@ -9,7 +9,7 @@ namespace MODELS
         public String Nombre { get; set; }
         public String Descripcion { get; set; }
         public int DuracionMeses { get; set; }
-        public double Precio { get; set; }
+        public decimal Precio { get; set; }
     }    
 }
 
